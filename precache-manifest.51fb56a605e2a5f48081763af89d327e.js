d6c3e4cfb71cdb3163c4ce0dcd4687f9ca049022
Nikhil Shankar <nikhil_shankar@outlook.com>
2020-07-22 13:45:19 -0400

    Updates

---
+++ precache-manifest.51fb56a605e2a5f48081763af89d327e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2a3c461f8a2cbca8084a51a902c540",
+    "url": "/ReactTetris/index.html"
+  },
+  {
+    "revision": "58b0fe59a11a62e787f5",
+    "url": "/ReactTetris/static/css/main.6fee1b22.chunk.css"
+  },
+  {
+    "revision": "60b395ba2ed10c80de26",
+    "url": "/ReactTetris/static/js/2.271727f0.chunk.js"
+  },
+  {
+    "revision": "58b0fe59a11a62e787f5",
+    "url": "/ReactTetris/static/js/main.c466a8dc.chunk.js"
+  },
+  {
+    "revision": "d2b4aa02706a0c11b8d1",
+    "url": "/ReactTetris/static/js/runtime~main.57c4be56.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/ReactTetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "bc018adc89901eaadb3dd45a881200be",
+    "url": "/ReactTetris/static/media/bg.bc018adc.png"
+  }
+]);
